bcb1194fd7eb3feb2c4ecb922400506f3e159a49
kimballmars <chenkimball@gmail.com>
2024-12-07 03:55:03 -0500

    update cartslice

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,6 +1,9 @@
 import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { addItem } from './CartSlice';
+import { useDispatch } from 'react-redux';
+
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
@@ -245,6 +248,17 @@ const handlePlantsClick = (e) => {
    const handleContinueShopping = (e) => {
     e.preventDefault();
     setShowCart(false);
+  };
+
+  const [addedToCart, setAddedToCart] = useState({});
+
+  const handleAddToCart = (plant) => {
+    const dispatch = useDispatch();
+    dispatch(addItem(plant));
+    setAddedToCart((prevState) => ({
+        ...prevState,
+        [plant.name]: true,
+    }));
   };
     return (
         <div>
